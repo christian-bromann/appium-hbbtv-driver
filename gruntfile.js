d214dd5272456135c1ab4c4cef8cb0777ae72a62
christian-bromann <contact@christian-bromann.com>
2016-08-03 14:25:59 +0200

    added boilerplate files

---
+++ gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkgFile: 'package.json',
+        clean: ['build'],
+        babel: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: './',
+                    src: ['index.js', './lib/**/*'],
+                    dest: 'build',
+                    ext: '.js'
+                }]
+            }
+        },
+        eslint: {
+            options: {
+                parser: 'babel-eslint'
+            },
+            target: ['lib/reporter.js']
+        },
+        contributors: {
+            options: {
+                commitMessage: 'update contributors'
+            }
+        },
+        bump: {
+            options: {
+                commitMessage: 'v%VERSION%',
+                pushTo: 'upstream'
+            }
+        },
+        watch: {
+            dist: {
+                files: './lib/**/*.js',
+                tasks: ['babel:dist']
+            }
+        }
+    })
+
+    require('load-grunt-tasks')(grunt)
+    grunt.registerTask('default', ['eslint', 'build', 'mocha_istanbul'])
+    grunt.registerTask('build', 'Build wdio-allure-reporter', function () {
+        grunt.task.run([
+            'clean',
+            'babel'
+        ])
+    })
+    grunt.registerTask('release', 'Bump and tag version', function (type) {
+        grunt.task.run([
+            'build',
+            'contributors',
+            'bump:' + (type || 'patch')
+        ])
+    })
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+import yargs from 'yargs'
+import { HbbTVDriver } from './lib/driver'
+
+const DEFAULT_HOST = "localhost";
+const DEFAULT_PORT = 4723;
+
+async function main () {
+  let port = yargs.argv.port || DEFAULT_PORT;
+  let host = yargs.argv.host || DEFAULT_HOST;
+  // return startServer(port, host);
+}
+
+export default IosDriver;
