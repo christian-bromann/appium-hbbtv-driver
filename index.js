72c22af31e0242ea48fbb5fa9cdc7547ddbed600
christian-bromann <contact@christian-bromann.com>
2016-08-03 14:47:52 +0200

    added server package

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import yargs from 'yargs'
 import { HbbTVDriver } from './lib/driver'
+import { startServer } from './server'
 
-const DEFAULT_HOST = "localhost";
-const DEFAULT_PORT = 4723;
+const DEFAULT_HOST = 'localhost'
+const DEFAULT_PORT = 4723
 
 async function main () {
-  let port = yargs.argv.port || DEFAULT_PORT;
-  let host = yargs.argv.host || DEFAULT_HOST;
-  // return startServer(port, host);
+    let port = yargs.argv.port || DEFAULT_PORT
+    let host = yargs.argv.host || DEFAULT_HOST
+    return startServer(port, host)
 }
 
-export default IosDriver;
+export default HbbTVDriver
