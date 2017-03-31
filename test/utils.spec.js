4341f47d6375244441771984ddf38afecf5ad802
christian-bromann <mail@christian-bromann.com>
2017-03-31 11:59:58 +0200

    complete utils unit tests

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,7 +1,13 @@
+import fs from 'fs'
+import path from 'path'
 import { expect } from 'chai'
 import {
     getDomain,
-    hasGzipEncoding
+    hasGzipEncoding,
+    getDescription,
+    getIpAddress,
+    writeConfig,
+    readConfig
 } from '../lib/utils'
 
 describe('utils', () => {
@@ -17,5 +23,48 @@ describe('utils', () => {
         expect(hasGzipEncoding({ headers: { 'accept-encoding': 'gzip deflate' } })).to.be.ok
         expect(hasGzipEncoding({ headers: { 'accept-encoding': 'foobar' } })).to.be.not.ok
         expect(hasGzipEncoding({ headers: {} })).to.be.not.ok
+    })
+
+    it('getDescription', () => {
+        let html = `<head><meta name="description" content="foobar" /></head>`
+        expect(getDescription(html)).to.be.equal('foobar')
+        html = `<head><meta content="foobar" name="description" /></head>`
+        expect(getDescription(html)).to.be.equal('foobar')
+        html = `<head><meta content="foobar" data-foo="bar" name="description" /></head>`
+        expect(getDescription(html)).to.be.equal('foobar')
+        html = `<head></head>`
+        expect(getDescription(html)).to.be.equal(undefined)
+    })
+
+    it('getIpAddress', () => {
+        expect(getIpAddress()).to.be.equal(null)
+        expect(getIpAddress('lo0')).to.be.equal('127.0.0.1')
+        expect(getIpAddress('lo0', 'ipv6')).to.be.equal('::1')
+    })
+
+    describe('should be able to read and write to config file', () => {
+        const configPath = path.resolve(__dirname, '..', 'config.json')
+
+        it('should return undefined if config is not available', () => {
+            const data = readConfig()
+            expect(data).to.be.a('object')
+            expect(JSON.stringify(data)).to.be.equal('{"data":{}}')
+        })
+
+        it('writeConfig', () => {
+            writeConfig('jojo')
+            expect(fs.existsSync(configPath)).to.be.ok
+        })
+
+        it('readConfig', () => {
+            const data = readConfig()
+            expect(data).to.be.a('object')
+            expect(JSON.stringify(data)).to.be.equal('{"data":"jojo"}')
+            expect(fs.existsSync(configPath)).to.be.ok
+        })
+
+        after(() => {
+            fs.unlinkSync(configPath)
+        })
     })
 })
