3b7f1a1968a4b3f191ebcc355dba7c3b310a4c01
christian-bromann <mail@christian-bromann.com>
2017-05-04 12:15:31 +0200

    remove webpack

--- config/webpack.config/index.js
+++
@@ -1,7 +0,0 @@
-require('babel-register')
-const webpackValidator = require('webpack-validator')
-
-const ENV = process.env.NODE_ENV || 'development'
-const CONFIG = require(`./${ENV}`)
-
-module.exports = webpackValidator(CONFIG)
