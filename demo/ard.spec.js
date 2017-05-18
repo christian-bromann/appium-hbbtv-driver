c967a33a14b3127fc93b35a068675401ab24acf8
christian-bromann <mail@christian-bromann.com>
2017-05-18 18:26:20 +0200

    more demo tests

--- demo/ard.spec.js
+++ demo/ard.spec.js
@@ -22,16 +22,62 @@ describe('ARD HbbTV app', () => {
         })
 
         it('should display Mediathek app', () => {
-            browser.pause(1000)
-            expect(browser.getText('.barbottom .seltxt')).to.be.equal('Das Erste Mediathek')
+            expect($('.barbottom .seltxt').getText()).to.be.equal('Das Erste Mediathek')
         })
 
         it('should switch to Tatort', () => {
-            browser.pause(1000)
             browser.keys('left')
-            browser.pause(1000)
             browser.keys('left')
-            expect(browser.getText('.barbottom .seltxt')).to.be.equal('Der Tatort im Ersten')
+            expect($('.barbottom .seltxt').getText()).to.be.equal('Der Tatort im Ersten')
+        })
+    })
+
+    describe('has a settings menu', () => {
+        it('should not display settings per default', () => {
+            expect($('#appscreen #settings').isVisible()).to.be.equal(false)
+            expect($('#appscreen #bar').isVisible()).to.be.equal(true)
+        })
+
+        it('should open settings', () => {
+            browser.keys('down')
+            browser.keys('enter')
+            expect($('#appscreen #settings').isVisible()).to.be.equal(true)
+            expect($('#appscreen #bar').isVisible()).to.be.equal(false)
+        })
+
+        it('should enabled accessibility', () => {
+            browser.keys('down')
+            browser.keys('down')
+            browser.keys('down')
+            browser.keys('enter')
+            const buttonTxt = $('.barcolbuttons .colbuttontxt')
+            expect(buttonTxt.getText()).to.be.equal('Blau: Videotext')
+            expect(buttonTxt.getAttribute('class')).to.be.equal('colbuttontxt lg')
+        })
+
+        it('should store settings so they are available after reopening app', () => {
+            browser.url('http://itv.ard.de/ardstart/index.html')
+            $('#hidemsg').waitForVisible()
+            browser.keys('red')
+            $('#appscreen').waitForVisible()
+
+            const buttonTxt = $('.barcolbuttons .colbuttontxt')
+            expect(buttonTxt.getText()).to.be.equal('Blau: Videotext')
+            expect(buttonTxt.getAttribute('class')).to.be.equal('colbuttontxt lg')
+        })
+
+        it('should disable accessibility', () => {
+            // go back to settings page
+            browser.keys('down')
+            browser.keys('enter')
+
+            browser.keys('up')
+            browser.keys('up')
+            browser.keys('up')
+            browser.keys('enter')
+            const buttonTxt = $('.barcolbuttons .colbuttontxt')
+            expect(buttonTxt.getText()).to.be.equal('Videotext')
+            expect(buttonTxt.getAttribute('class')).to.be.equal('colbuttontxt')
         })
     })
 })
